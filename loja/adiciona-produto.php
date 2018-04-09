47058bf159dc5a817c695e6848d01c12f76fc950
ascom <repositoriofae@gmail.com>
2018-04-09 17:38:44 -0300

    Carregamento da descricao

---
+++ loja/adiciona-produto.php
@@ -0,0 +1,21 @@
+<?php include("cabecalho.php");
+      include("conecta.php");
+      include("banco-produto.php"); ?>
+
+<?php
+
+$nome = $_GET["nome"];
+$preco = $_GET["preco"];
+$descricao = $_GET["descricao"];
+
+if(insereProduto($conexao, $nome, $preco,$descricao)) { ?>
+    <p class="text-success">O produto <?= $nome; ?>, <?= $preco; ?> adicionado com sucesso!</p>
+<?php } else {
+    $msg = mysqli_error($conexao);
+?>
+    <p class="text-danger">O produto <?= $nome; ?> não foi adicionado: <?= $msg ?></p>
+<?php
+}
+?>
+
+<?php include("rodape.php"); ?>
---
+++ loja/produto-lista.php
@@ -0,0 +1,28 @@
+<?php include("cabecalho.php");
+      include("conecta.php");
+      include("banco-produto.php"); ?>
+
+<?php if(array_key_exists("removido", $_GET) && $_GET['removido']=='true') { ?>
+    <p class="alert-success">Produto apagado com sucesso.</p>
+<?php } ?>
+
+<table class="table table-striped table-bordered">
+
+    <?php
+        $produtos = listaProdutos($conexao);
+        foreach($produtos as $produto) :
+    ?>
+    <tr>
+        <td><?= $produto['nome'] ?></td>
+        <td><?= $produto['preco'] ?></td>
+        <td><?= $produto['descricao'] ?></td>
+        <td>
+            <a href="remove-produto.php?id=<?=$produto['id']?>" class="text-danger">remover</a>
+        </td>
+    </tr>
+    <?php
+        endforeach
+    ?>
+</table>
+
+<?php include("rodape.php"); ?>
